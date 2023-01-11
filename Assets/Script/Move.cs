c7e65f1ee938629aa3b075ab492db58c0fbcc797
LMZ <811876473@qq.com>
2023-01-11 11:52:24 +0800

    first

---
+++ Assets/Script/Move.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private Transform IMA;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IMA = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector2 pos = IMA.position;
+            pos.y += 0.5f;
+            IMA.position = pos;
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Vector2 pos = IMA.position;
+            pos.y -= 0.5f;
+            IMA.position = pos;
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector2 pos = IMA.position;
+            pos.x -= 0.5f;
+            IMA.position = pos;
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector2 pos = IMA.position;
+            pos.x += 0.5f;
+            IMA.position = pos;
+
+        }
+    }
+}
